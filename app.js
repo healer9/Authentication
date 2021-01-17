4afb0916532b598fc713130fe9ca2af518788104
healer9 <anasriyaz92@gmail.com>
2021-01-17 23:03:54 +0530

    Level 2 Mongoose-Encryption

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import ejs from 'ejs'
-import mongoose from 'mongoose'
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser') 
+const ejs = require('ejs') 
+const mongoose = require('mongoose') 
+const encrypt = require('mongoose-encryption') 
 
 const PORT = 3000
 const app = express()
@@ -12,10 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true })
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+})
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
 
 const User = mongoose.model('User', userSchema)
 
